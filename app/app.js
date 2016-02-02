466ac4e6292508360d1ab34020b464a6df52a555
MikeC1995 <mike_christensen@btinternet.com>
2016-02-02 19:16:12 +0000

    added ui routing for each tab

--- app/app.js
+++ app/app.js
@@ -1,7 +1,24 @@
 'use strict';
 
 // The root application module for this app
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("map", {
+      url:"/",
+      templateUrl:"/app/components/map/map.view.html"
+    })
+    .state("trips", {
+      url:"/trips",
+      templateUrl:"/app/components/trips/trips.view.html"
+    })
+    .state("friends", {
+      url:"/friends",
+      templateUrl:"/app/components/friends/friends.view.html"
+    });
+  $urlRouterProvider.otherwise("/");
+});
 
 app.controller('appController', function() {
 
