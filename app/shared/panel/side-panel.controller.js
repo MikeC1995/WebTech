acfff80523e278d939c8dd874bbcb6b115471ecb
MikeC1995 <mike_christensen@btinternet.com>
2016-02-03 13:55:21 +0000

    remove accidental duplicate folder

--- app/shared/panel/side-panel.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = angular.module('app');
-app.controller('sidePanelController', function($scope, $state) {
-  $scope.$state = $state;
-  $scope.test = function() {
-    console.log("test " + JSON.stringify($scope.$state.current));
-  }
-});
