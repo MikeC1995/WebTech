3ed5b5262ac2b1038160129d0680800633c2885f
MikeC1995 <mike_christensen@btinternet.com>
2016-02-13 19:52:25 +0000

    setup modals, started implenting addplace dialog on /trips/addplace route

--- public/app/components/trips/trips.js
+++ public/app/components/trips/trips.js
@@ -4,5 +4,19 @@
 var trips = angular.module('trips', ['ui.router', 'api']);
 
 trips.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/trips");
+  $stateProvider
+    // abstract sub-state of trips forms the dialog backdrop + holder
+    .state("trips.modal", {
+      views: {
+        'modal': { templateUrl:"/app/shared/modals/modal.view.html" }
+      },
+      abstract: true
+    })
+    // add place dialog
+    .state("trips.modal.addPlace", {
+      url: '/addplace',
+      views: {
+        'modal': { templateUrl:"/app/shared/modals/add-place/add-place.view.html" }
+      }
+    });
 });
