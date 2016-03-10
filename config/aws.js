42a6b01821fc101741ef0b2c6ea3b1b2245fddcf
MikeC1995 <mike_christensen@btinternet.com>
2016-03-10 15:34:23 +0000

    new aws access keys, not hard coded (security risk!) but set as env var

--- config/aws.js
+++ config/aws.js
@@ -2,8 +2,8 @@ var AWS = require('aws-sdk');
 
 module.exports = (function() {
   AWS.config.update({
-    accessKeyId: 'AKIAIUU2YST2U4QMKJUA',
-    secretAccessKey: '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'
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   });
   var s3 = new AWS.S3();
 
