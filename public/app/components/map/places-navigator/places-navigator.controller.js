53f42fd91415f4bcec40a644224989e5d461bda7
MikeC1995 <mike_christensen@btinternet.com>
2016-05-18 13:15:52 +0100

    places nav looks good if < 3 places

--- public/app/components/map/places-navigator/places-navigator.controller.js
+++ public/app/components/map/places-navigator/places-navigator.controller.js
@@ -3,6 +3,11 @@
 var map = angular.module('map');
 map.controller('placesNavigatorController', ['$scope', function($scope) {
   $scope.$watch('selectedPlaceIndex', function(value) {
-    $scope.transform = 'translateX(' + (-100 * value + 100) + '%)';
+    $scope.transform = 'none';
+    if($scope.filteredPlaces) {
+      if($scope.filteredPlaces.length >= 3) {
+        $scope.transform = 'translateX(' + (-100 * value + 100) + '%)';
+      }
+    }
   });
 }]);
