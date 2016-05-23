42c98dab186e23a368a6c904224a517e86f481c2
MikeC1995 <mike_christensen@btinternet.com>
2016-05-23 15:02:42 +0100

    login button on nav when not logged in

--- apps/public/app/components/map/image-viewer/image-viewer.controller.js
+++ apps/public/app/components/map/image-viewer/image-viewer.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 var map = angular.module('map');
-map.controller('imageViewerController', ['$scope', function($scope) {
+map.controller('imageViewerController', ['$scope', '$stateParams', 'userFactory', function($scope, $stateParams, userFactory) {
+  // Returns true if the map being viewed is not the authenticated user's
+  $scope.me = {};
+  userFactory.getMe().then(function(me) {
+    $scope.me = me;
+  }, function() {});
 
+  $scope.isMyMap = function() {
+    return $scope.me._id == $stateParams.user_id;
+  }
 }]);
